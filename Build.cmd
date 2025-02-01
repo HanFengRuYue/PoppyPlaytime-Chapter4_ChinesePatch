0a8e28dea48b28747b462207a0ce6c45ecbdb7ab
HanFengRuYue <huangkanggg@gmail.com>
2025-02-01 21:03:23 +0800

    测试工作流

--- Build.cmd
+++ Build.cmd
@@ -1,5 +1,3 @@
-UnrealPakSwitchv10\UnrealLocres.exe import UnrealPakSwitchv10\Game.locres Game.csv
-move "UnrealPakSwitchv10\Game.locres.new" "UnrealPakSwitchv10\ch4_pro\Content\Localization\Game\en\"
-del "UnrealPakSwitchv10\ch4_pro\Content\Localization\Game\en\Game.locres"
-rename "UnrealPakSwitchv10\ch4_pro\Content\Localization\Game\en\Game.locres.new" "Game.locres"
-UnrealPakSwitchv10\v5.3\2\3\UnrealPak.exe ch4_pro-Windows_p.pak -Create=ch4_pro.txt -compress
+UnrealPakSwitchv10\UnrealLocres.exe import Game.locres Game.csv -o Game.locres
+move /Y "UnrealPakSwitchv10\Game.locres" "UnrealPakSwitchv10\ch4_pro\Content\Localization\Game\en\Game.locres"
+UnrealPakSwitchv10\v5.3\2\3\UnrealPak.exe ..\..\..\ch4_pro-Windows_p.pak -Create=ch4_pro.txt -compress
