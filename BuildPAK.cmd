1e35abd801457d296c4a0e2b00be16e0e8c34e4b
HanFengRuYue <huangkanggg@gmail.com>
2025-02-01 23:38:10 +0800

    Test

--- BuildPAK.cmd
+++ BuildPAK.cmd
@@ -1,3 +1,3 @@
 scr\UnrealLocres.exe import Game.locres Game.csv -o scr\Game.locres
 move /Y "scr\Game.locres" "scr\ch4_pro\Content\Localization\Game\en\Game.locres"
-scr\v11.27\2\3\UnrealPak.exe ..\..\..\ch4_pro-Windows_p.pak -Create=lista.txt -compressed
+scr\v11.27\2\3\1UnrealPak.exe ..\..\..\ch4_pro-Windows_p.pak -Create=..\..\..\lista.txt -compress
