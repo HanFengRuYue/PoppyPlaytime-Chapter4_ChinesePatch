8db322c7090037d0490220d294af73794b4090a5
HanFengRuYue <huangkanggg@gmail.com>
2025-02-01 21:04:38 +0800

    Update Build.cmd

--- Build.cmd
+++ Build.cmd
@@ -1,3 +1,3 @@
 UnrealPakSwitchv10\UnrealLocres.exe import Game.locres Game.csv -o Game.locres
-move /Y "UnrealPakSwitchv10\Game.locres" "UnrealPakSwitchv10\ch4_pro\Content\Localization\Game\en\Game.locres"
+move /Y "Game.locres" "UnrealPakSwitchv10\ch4_pro\Content\Localization\Game\en\Game.locres"
 UnrealPakSwitchv10\v5.3\2\3\UnrealPak.exe ..\..\..\ch4_pro-Windows_p.pak -Create=ch4_pro.txt -compress
